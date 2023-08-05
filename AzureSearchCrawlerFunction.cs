a136a42f7285523b9ff754c982999f515b419676
urruegg <urs.ruegg@microsoft.com>
2023-08-05 14:02:47 +0200

    Initial

---
+++ AzureSearchCrawlerFunction.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Teamruegg.AzureSearchCrawler
+{
+    public class AzureSearchCrawlerFunction
+    {
+        private readonly ILogger _logger;
+
+        public AzureSearchCrawlerFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AzureSearchCrawlerFunction>();
+        }
+
+        [Function("AzureSearchCrawlerFunction")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
